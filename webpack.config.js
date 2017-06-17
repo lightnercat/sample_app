fa18f5b0a1fdc66de0cee981d3de361e58be282d
Nakamura Kosuke <ncl0709@gmail.com>
2017-06-18 01:24:43 +0900

    yarn add vue vue-loader yarn add babel-loader babel-preset-es2015 -dev

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,30 @@
 module.exports = {
-  entry: __dirname + "/frontend/javascripts/top/a.js",
+  entry: {
+    top: __dirname + "/frontend/javascripts/top/index.js",
+    login: __dirname + "/frontend/javascripts/login/index.js",
+  },
   output: {
     path: __dirname + '/app/assets/javascripts',
-    filename: 'top.js'
+    filename: '[name].js'
   },
+  module: {
+    loaders: [
+      {
+        test: /\.is$/,
+        exclude: /node_module/,
+        loader: 'babel-loader'
+      },
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader'
+      }
+    ]
+  },
+  resolve: {
+    alias: {
+      'vue$':        'vue/dist/vue.js',
+      'vue-router$': 'vue-router/dist/vue-router.js'
+    },
+    extensions: ['.js']
+  }
 };
